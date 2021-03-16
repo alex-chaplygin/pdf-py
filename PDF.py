9d9a65867ccd85500998f7076302083f51a7c166
alex-chaplygin <alex_chaplygin@mail.ru>
2021-03-16 16:27:34 +0300

    метод разбор заголовка

--- PDF.py
+++ PDF.py
@@ -4,9 +4,6 @@ class PDF(object):
 
   """
    Содержит все данные из PDF файла
-
-  :version:
-  :author:
   """
 
   def __init__(self, filename):
@@ -14,25 +11,33 @@ class PDF(object):
      Читает PDF файл, загружает все данные
 
     @param string filename : имя файла
-    @return  :
-    @author
     """
     print(filename)
     f = open(filename[0], 'rb')
     self.data = f.read()
     f.close()
+    self.version = self.getVersion()
+    print(self.version)
 
   def getPage(self, page):
     """
      Возвращает список графических примитивов
 
-    @param int page : номер страницы
-    @return list :
-    @author
+    @param page : номер страницы
+    @return : список графических примитивов
     """
     print(self.data)
     lines = [(0, 0, 500, 500), (70, 100, 350, 200), (20, 460, 400, 10,), (150, 100, 400, 80)]
     return lines
 
 
-
+  def getVersion(self):
+    """
+     Возвращает версию PDF как строку
+    self.data - данные из файла
+    Алгоритм:
+    разбить данные на строки
+    из первой строки извлечь версию
+    удалить первую строку из списка строк
+    """
+    pass
