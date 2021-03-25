df83267ad468925689ea554b4dd4eb74d3643132
alex-chaplygin <alex_chaplygin@mail.ru>
2021-03-25 09:49:20 +0300

    Проектирование класса Stream

--- Stream.py
+++ Stream.py
@@ -13,11 +13,64 @@ class Stream(object):
 
   """ ATTRIBUTES
 
-   список байт
+   stream - список байт
 
-  data  (private)
+  dictionary - словарь атрибутов
 
   """
+  def __init__(self, dictionary, stream):
+    """
+    Создает объект Stream на основе словаря dictionary и строки байт stream
+    В словаре по ключу /Length проверяется длина потока
+    """
+    pass
 
+  def decode(self):
+    """
+    Декодирует поток в зависимости от /Filter
+    Если есть /DL то проверяется длина декодированной строки
+    @return : декодированная строка
+    """
+    pass
 
+  def decode_lzw(self):
+    """
+    @return : декодированная строка
+    """
+    pass
 
+  def decode_ascii85(self):
+    """
+    @return : декодированная строка
+    """
+    pass
+  
+  def decode_flate(self):
+    """
+    @return : декодированная строка
+    """
+    pass
+
+  def decode_runlength(self):
+    """
+    @return : декодированная строка
+    """
+    pass
+
+  def decode_ccitt(self):
+    """
+    @return : декодированная строка
+    """
+    pass
+  
+  def decode_jbig2(self):
+    """
+    @return : декодированная строка
+    """
+    pass
+
+  def decode_dct(self):
+    """
+    @return : декодированная строка
+    """
+    pass
