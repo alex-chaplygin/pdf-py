986bd006effe33d2a5979ac2be259e6fb9bccbdb
alex-chaplygin <alex_chaplygin@mail.ru>
2021-03-26 07:28:23 +0300

    Главный класс программы

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 from PDF import *
 from tkinter import *
 from tkinter import filedialog
-window = Tk()
-window.title('Test1')
-window.geometry('500x500')
-canvas = Canvas(width=500, height=500)
-canvas.grid(column=0, row=0)
-file = filedialog.askopenfilenames()
-pdf = PDF(file)
-for elem in pdf.getPage(1):
-    canvas.create_line(elem)
-menu = Menu(window)
-window.config(menu=menu)
 
 
-def fileOpen():
-    pdf.getPage(1)
+class Main:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Test1')
+        self.window.geometry('500x500')
+        self.canvas = Canvas(width=500, height=500)
+        self.canvas.grid(column=0, row=0)
+        menu = Menu(self.window)
+        self.window.config(menu=menu)
+        menu.add_command(label='Открыть', command=self.open)
+        menu.add_command(label='Предыдущая страница', command=self.next)
+        menu.add_command(label='Следующая страница', command=self.previous)
+        self.page=1
+        self.pdf = None
+
+        
+    def run(self):
+        file = filedialog.askopenfilenames()
+        self.pdf = PDF(file)
+        for elem in self.pdf.getPage(1):
+            self.canvas.create_line(elem)
+
+        self.window.mainloop()
 
 
-menu.add_command(label='Открыть', command=fileOpen)
-menu.add_command(label='Контакты')
-window.mainloop()
+    def open(self):
+        file = filedialog.askopenfilenames()
+        self.pdf = PDF(file)
+        self.pdf.getPage(self.page)
+
+        
+    def next(self):
+        self.page += 1
+        self.pdf.getPage(self.page)
+
+
+    def previous(self):
+        self.page -= 1
+        self.pdf.getPage(self.page)
+
+        
+main = Main()
+main.run()
