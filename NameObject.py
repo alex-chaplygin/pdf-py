7b5579a0cabacdd1c799e61722cf5fd724bb7bbe
alex-chaplygin <alex_chaplygin@mail.ru>
2021-04-20 16:33:28 +0300

    Представление, сравнение, хеш

--- NameObject.py
+++ NameObject.py
@@ -22,6 +22,22 @@ class NameObject(object):
     string - строка имени без слэша
     #xx - заменяется как символ с 16-ричным кодом
     """
-    pass
+    self.data = string
 
 
+  def __repr__(self):
+    """
+    представление объекта в виде строки
+    """
+    return '/' + self.data
+
+
+  def __eq__(self, other):
+    """
+    метод сравнения объектов
+    """
+    return self.data == other.data
+
+
+  def __hash__(self):
+    return hash(self.data)
