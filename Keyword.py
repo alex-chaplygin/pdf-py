88b353f5699c21b3b4500530f4ed0988104f8115
alex-chaplygin <alex_chaplygin@mail.ru>
2021-04-20 16:32:56 +0300

    Представление и сравнение

--- Keyword.py
+++ Keyword.py
@@ -4,4 +4,16 @@ class Keyword:
     data - строка (obj endobj stream endstream R xref trailer)
     """
     def __init__(self, string):
-        pass
+        self.data = string
+
+
+    def __repr__(self):
+        return 'Key:' + self.data
+
+
+    def __eq__(self, other):
+        """
+        метод сравнения объектов
+        """
+        return self.data == other.data
+    
