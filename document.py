c6a32d17046ad7aff8b5349f8f8b4afdfad8c272
alex-chaplygin <alex_chaplygin@mail.ru>
2021-05-27 05:59:07 +0300

    упрощение интерфейса

--- document.py
+++ document.py
@@ -17,8 +17,10 @@ from collections import namedtuple
 import pdf_file
 
 
-# дерево страниц
-page_tree = []
+# список страниц
+page_list = []
+# кэш страниц (страницы вместе с содержимым)
+page_cache = {}
 
 # класс для страниц
 # p1 = Page(resources=(100, 0), contents=(200, 0))
@@ -32,56 +34,38 @@ def load(file_name):
     file_name - имя файла
     формирует дерево страниц
     '''
-    global page_tree
+    global page_list
     pdf_file.load(file_name)
     catalog = pdf_file.get_object(pdf_file.root_ref)
     if catalog.get('Type') != NameObject('Catalog'):
         raise Exception('No catalog')
     pages = pdf_file.get_object(catalog.get('Pages'))
-    page_tree = load_tree(pages)
+    load_pages(pages)
 
 
-def load_tree(node):
+def load_pages(node):
     '''
-    загружает дерево страниц из узла
+    загружает дерево страниц из узла и формирует список страниц
 
     node - объект узел (Pages)
-    создаем пустой список
     проходим по всем элементам списка Kids
         загружаем объект по ссылке
         если тип объекта - Page, то создаем страницу и добавляем в список
-        иначе если тип - Pages, вызываем load_tree для этого узла и добавляем результат в список
+        иначе если тип - Pages, вызываем load_pages для этого узла
         иначе ошибка
-    возвращает: список страниц
     '''
+    global page_list
     pass
 
 
 def get_page(num):
     '''
-    загрузка страницы
+    загрузка содержимого страницы
 
     num - номер страницы (начиная с 1)
-    ищет страницу в дереве страниц
-    возвращает страницу Page
+    если в contents - 1 объект, то загружаем его и присваиваем в contents
+    если список, то загружаем все объекты и накапливаем содержимое в contents
+    возвращает страницу Page с загруженным содержимым (вместо кортежа - строка байт)
     '''
-    page, count = get_page_from_node(page_tree, num, 1)
+    page, count = get_page_from_node(page_list, num, 1)
     return page
-
-
-def get_page_from_node(node, page_num, num):
-    '''
-    поиск страницы в узле
-
-    node - узел список, в котором ищем
-    page_num - номер страницы, которую ищем
-    num - счетчик страниц
-    обходим все элементы списка node
-       если текущий элемент списка - список, тогда вызываем get_page_from_node с элементом, номером страницы, счетчик + 1
-                          если страница не была найдена, то увеличить счетчик на count
-       если номер страницы равен счетчику то возвращаем страницу (элемент, 0)
-       увеличиваем счетчик num
-    после цикла вернуть (None, число элементов в списке) - не найдено
-    возвращает: (страницу, число элементов если узел)
-    '''
-    pass
