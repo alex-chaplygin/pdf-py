73ca5f19f5c04ee017697ecac77ac4bbd216b84e
alex-chaplygin <alex_chaplygin@mail.ru>
2021-06-11 12:58:35 +0300

    Реализация функций

--- font.py
+++ font.py
@@ -1,3 +1,6 @@
+import tokens
+import pdf_parser as parser
+import pdf_file
 # словарь подстановок для текущего шрифта
 cmap = {}
 
@@ -9,14 +12,30 @@ def load(ref):
     ref - ссылка на шрифт
     загрузка cmap
     '''
-    pass
+    global cmap
+    font = pdf_file.get_object(ref)
+    m = font.get('ToUnicode')
+    cmap.clear()
+    if m != None:
+        old = tokens.get_char
+        old_c = tokens.cur_char
+        old_t = parser.cur_token
+        tokens.get_char = pdf_file.get_char
+        obj = pdf_file.get_object(m).stream
+        tokens.get_char = old
+        tokens.cur_char = old_c
+        parser.cur_token = old_t
+        load_cmap(obj)
 
 
-def load_cmap(obj):
+stream_pos = 0
+        
+
+def load_cmap(stream):
     '''
     загрузка словаря подстановки
 
-    obj - объект подстановки
+    stream - поток объекта подстановки
     ...
     endcodespacerange
     4 beginbfrange
@@ -32,11 +51,64 @@ def load_cmap(obj):
     endbfchar
     '''
     global cmap
-    pass
+    global stream_pos
+#    print(stream)
+    stream_pos = 0
 
 
+    def get_stream_char():
+        global stream_pos
+        stream_pos += 1
+        if stream_pos  > len(stream):
+            return -1
+        else:
+            return chr(stream[stream_pos - 1])
+
+    old = tokens.get_char
+    old_c = tokens.cur_char
+    old_t = parser.cur_token
+    tokens.get_char = get_stream_char
+    tokens.cur_char = tokens.get_char()
+    parser.cur_token = tokens.get_token()
+
+    data = ''
+    while data != 'endcodespacerange':
+        data = parser.parse_data()
+    while parser.cur_token != ('id', 'endcmap'):
+        num = parser.parse_data()
+        typ = parser.parse_data()
+        for i in range(num):
+            if typ == 'beginbfrange':
+                start = parser.parse_data()[0]
+                end = parser.parse_data()[0]
+                data = parser.parse_data()
+                if len(data) == 1:
+                    data = chr(data[0])
+                else:
+                    data = data.decode('utf-16be')
+                code_start = ord(data)
+#                print(start, end, code_start)
+                for i in range(start, end + 1):
+                    cmap[i] = chr(code_start + i - start)
+            elif typ == 'beginbfchar':
+                code = parser.parse_data()[0]
+                data = parser.parse_data().decode('utf-16be')
+                cmap[code] = data
+        parser.parse_data()
+#    print(cmap)
+    tokens.get_char = old
+    tokens.cur_char = old_c
+    parser.cur_token = old_t
+    
+    
 def get_char(code):
     '''
     возвращает символ по коду
     '''
-    pass
+    if ord(code) in cmap:
+#        print(code, ord(code), cmap[ord(code)])
+        if ord(cmap[ord(code)]) > 0xffff:
+            return ' '
+        return cmap[ord(code)]
+    else:
+        return code
